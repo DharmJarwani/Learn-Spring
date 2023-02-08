d159c1a97261d9799ec55b41600593edcb10f8a0
Dharm <99967659+DharmJarwani@users.noreply.github.com>
2023-02-08 17:38:19 +0530

    Vid 26 finished How to invoke static method , variables and creating object | Spring Expression Language

--- springcore/src/main/java/com/springcore/spel/Demo.java
+++ springcore/src/main/java/com/springcore/spel/Demo.java
@@ -11,6 +11,30 @@ public class Demo {
 	@Value("#{2+5+56+34}")
 	private int y;
 	
+	// This is the way to call static method
+	@Value("#{T(java.lang.Math).sqrt(144)}")
+	private double z;
+	
+	// This is the way to call static variable
+	@Value("#{T(java.lang.Math).E}")
+	private double E;
+	
+	// This is the way to create object with expresstion
+	@Value("#{ new java.lang.String('Durgesh Tiwari')}")
+	private String name;
+	
+	public double getE() {
+		return E;
+	}
+	public void setE(double e) {
+		E = e;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
 	public int getX() {
 		return x;
 	}
@@ -23,9 +47,20 @@ public class Demo {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 	@Override
 	public String toString() {
-		return "Demo [x=" + x + ", y=" + y + "]";
+		return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", E=" + E + ", name=" + name + "]";
 	}
+	
+	
+	
+	
+	
 
 }
